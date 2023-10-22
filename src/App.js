5d59ce48195e351e8f8e344f1a652d1657d3a635
Mihai Fratila <mihai.fratila01@gmail.com>
2023-10-22 19:07:08 +0300

    Added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,66 @@
 import './App.css';
 import api from './api/axiosConfig.js'
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
 
     try {
       const response = await api.get("/api/v1/movies");
+      console.log(response.data);
       setMovies(response.data);
-    } 
-    catch(error) {
+    }
+    catch (error) {
       console.log(error);
     }
   }
 
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+  
+      const reviewsResponse = await api.get(`api/v1/reviews/${movieId}`)
+      setReviews(reviewsResponse.data);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
   useEffect(() => {
     getMovies();
-  },[])
+  }, [])
 
   return (
     <div className="App">
-
+      <Header />
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />}></Route>
-
+          <Route path="/" element={<Home movies={movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route path="/Reviews/:movieId" element={
+            <Reviews getMovieData={getMovieData}
+              movie={movie}
+              reviews={reviews}
+              setReviews={setReviews}
+            />
+          }
+          >
+          </Route>
         </Route>
       </Routes>
 
